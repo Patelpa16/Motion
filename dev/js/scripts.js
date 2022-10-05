4547984b7f2c2d4aca3fe12b827a3ab480b46e63
Patelpa16 <45794372+Patelpa16@users.noreply.github.com>
2022-10-04 22:45:35 -0400

    finishedhtmlandscss

---
+++ dev/js/scripts.js
@@ -0,0 +1,15 @@
+import {
+    gsap
+} from "gsap";
+
+gsap.from("#line-1", {
+    duration: 1,
+    x: -200,
+    alpha: 0
+});
+gsap.from("#line-2", {
+    duration: 1,
+    x: -200,
+    alpha: 0,
+    delay: 0.5
+});
