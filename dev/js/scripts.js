87294668c5f0f7a6c5b1ca702ae3d56f425a3ee3
Patelpa16 <45794372+Patelpa16@users.noreply.github.com>
2022-10-05 21:15:18 -0400

    animationdone

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,7 @@
 import {gsap} from "gsap";
 
-gsap.from("#line-1",{duration:1,x:-200,alpha:0});
-gsap.from("#line-2",{duration:1,x:-200,alpha:0,delay:0.5});
+gsap.from("#line-1",{duration:.7,x:-200,alpha:0});
+gsap.from("#line-2",{duration:.7,x:-200,alpha:0,delay:0.1});
+gsap.from("#portrait",{duration:1,x: +200,alpha:0});
+
+gsap.from("#resumebutton",{duration:.7,x: -200,alpha:0,delay:0.3});
