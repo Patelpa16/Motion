9a4f729a7a57fbffff47ffe9aa3096b2f1d5ea8c
Patelpa16 <45794372+Patelpa16@users.noreply.github.com>
2022-10-17 19:54:20 -0400

    changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,20 +1,3 @@
 import { gsap } from "gsap";
 
-gsap.from("#line-2",{duration:.7,x:-200,alpha:0});
-
-import $ from "jquery";
-
-let $nav = $('nav[data-nav]');
-let isVisible = false;
-
-$(".nav-btns").on("click", function(){
-    console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
-})
+gsap.from("#history-middle", {duration:.7,x:-200,alpha0});
