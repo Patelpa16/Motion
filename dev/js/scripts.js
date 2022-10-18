04e2d1f279a8c7274bb7a2000e5fc43498ca9ac5
Patelpa16 <45794372+Patelpa16@users.noreply.github.com>
2022-10-18 00:45:13 -0400

    addedanimation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,4 +3,55 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to("#history-middle", {duration:1,x:200,scrollTrigger:{trigger: "#history-middle"}});
+
+
+// gsap.to("#history-first", {
+//   backgroundPositionX: "0%",
+//   stagger: 1,
+//   scrollTrigger: {
+//     trigger: ".text",
+//     markers: true,
+//     scrub: 1,
+//     start: "top center",
+//     end: "bottom center"
+//   }
+// });
+
+
+
+gsap.from("#history-first", {
+  scrollTrigger: {
+    trigger: ".orange",
+    scrub: true,
+    pin: true,
+    start: "top top",
+    end: "+=100%",
+    markers: true
+  },
+  scaleX: 0, 
+  transformOrigin: "left center", 
+  ease: "none"
+});
+
+
+gsap.from("#emotion", {
+  scrollTrigger: {
+    trigger: "#emotion",
+    scrub: true,
+    start: "top bottom",
+    end: "top top",
+    markers: true
+  },
+    scaleX: 0,
+  transformOrigin: "left center", 
+  ease: "none"
+});
+
+
+
+//gsap.to("#history-middle", {
+//    scrollTrigger:{
+ //       trigger: "#history-middle",
+  //      start: "botttom center"
+  //  }
+   // });
