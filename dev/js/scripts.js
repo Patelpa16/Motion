d1aed46f9ecec976390de5ba238f744871fbce42
Patelpa16 <45794372+Patelpa16@users.noreply.github.com>
2022-10-19 00:30:15 -0400

    addedimganimation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,6 +7,20 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.from("#yellowbar", {duration: 1, alphay:-70})
 
 
+gsap.to('#imgrobot', {
+  scale: 1.5,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '#imgrobot',
+    markers: true,
+    start: "top center",
+  end: "bottom center"
+    
+
+  }
+})
+
+
 
 // gsap.to("#history-first", {
 //   backgroundPositionX: "0%",
@@ -22,33 +36,35 @@ gsap.from("#yellowbar", {duration: 1, alphay:-70})
 
 
 
-gsap.from("#history-first", {
-  scrollTrigger: {
-    trigger: ".orange",
-    scrub: true,
-    pin: true,
-    start: "top top",
-    end: "+=100%",
-    markers: true
-  },
-  scaleX: 0, 
-  transformOrigin: "left center", 
-  ease: "none"
-});
+// gsap.from("#yellow", {
+//   scrollTrigger: {
+//     trigger: ".orange",
+//     scrub: true,
+//     pin: true,
+//     start: "125px 150px",
+//     end: "+=100%",
+//     markers: true
+//   },
+//   scaleX: 0, 
+//   transformOrigin: "left center", 
+//   ease: "none"
+// });
 
 
-gsap.from("#yellow-bar", {
-  scrollTrigger: {
-    trigger: "#",
-    scrub: true,
-    start: "top bottom",
-    end: "top top",
-    markers: true
-  },
-    scaleX: 0,
-  transformOrigin: "left center", 
-  ease: "none"
-});
+
+
+// gsap.from("#emotion", {
+//   scrollTrigger: {
+//     trigger: "#",
+//     scrub: true,
+//     start: "top bottom",
+//     end: "top top",
+//     markers: true
+//   },
+//     scaleX: 0,
+//   transformOrigin: "left center", 
+//   ease: "none"
+// });
 
 
 //gsap.to("#history-middle", {
