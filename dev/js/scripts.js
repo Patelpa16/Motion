e4cdf5fcade01acaafe25149bb3f39c55dcdf809
Patelpa16 <45794372+Patelpa16@users.noreply.github.com>
2022-10-24 23:45:08 -0400

    changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -30,8 +30,76 @@ function scrollRevealAnimation() {
     )
     .to(".scroll-to-reveal", { opacity: 0 }, "first")
     .to("#svg", { opacity: 0 }, "-=.75");
-
 }
+
+function landingAnimation() {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#landing-section",
+      start: "-=60%",
+      markers: false
+    }
+  });
+
+  tl.from("#hero", {
+    clipPath: "inset(0 0 100% 0)",
+    ease: "expo.inOut",
+    duration: 2
+  }).from(
+    ["#imgrobot-2", "#early-history-2", "#history-header-2", "#yellow-2"],
+    {
+      stagger: 0.1,
+      opacity: 0
+    },
+    "-=1"
+  );
+}
+function historyAnimation() {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#history",
+      start: "-=15% center",
+      end: "center-=50 center",
+      markers: false,
+      scrub: 1
+    }
+  });
+
+  tl.from("#first-paragraph", { opacity: 0, y: 100, duration: 0.5 })
+    .from("#second-paragraph", { opacity: 0, y: 100, duration: 0.5 })
+    .from("#history-last", { opacity: 0, y: 100, duration: 0.5 });
+}
+function skillsAnimation() {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#skills",
+      start: "-=15% center",
+      end: "center-=50 center",
+      markers: false,
+      scrub: 1
+    }
+  });
+
+  tl.to("#skills-picture", {
+    backgroundPositionX: "-100px",
+    duration: 3
+  })
+    .from("#skills-picture", { clipPath: "inset(0 0 0 100%)" }, "<")
+    .from("#skills-content", { clipPath: "inset(0 0 0 100%)" }, "<")
+    .from(
+      ["#skills-header", "#skills-subheader"],
+      { opacity: 0, x: 75, stagger: 0.3 },
+      "-=1"
+    )
+    .from(".bottom", { stagger: 0.3, width: 0 })
+    .from(".top", { opacity: 0, stagger: 0.3 }, "<")
+    .from(["#yellowbar-1", "#yellowbar-2", "#yellowbar-3"], {
+      width: 0,
+      stagger: 0.3
+    });
+}
+
+
 
 
 
@@ -100,3 +168,5 @@ var mainTimeline = gsap.timeline();
 mainTimeline
   .add(scrollRevealAnimation())
   .add(landingAnimation())
+  .add(historyAnimation())
+  .add(skillsAnimation())
