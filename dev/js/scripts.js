473d8c193e3ff3908960a87c74713ed97d7f6adf
Patelpa16 <45794372+Patelpa16@users.noreply.github.com>
2022-12-07 16:48:34 -0500

    finalchanges

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,15 @@
-// import { gsap } from "gsap";
-// import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-// import { GSDevTools } from "gsap/GSDevTools";
+import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
 
-// gsap.registerPlugin(
-//     DrawSVGPlugin,
-//     GSDevTools
-// );
-gsap.set("#spotify_circle", { drawSVG: "50% 50%", rotate: 90,transformOrigin: "50% 50%" });
-gsap.set("#spotify_t_l", { x:-40,y:-10, opacity: 0 });
-gsap.set("#spotify_m_l", { x: 40 , opacity: 0});
-gsap.set("#spotify_b_l", { x:-40,y: 5, opacity: 0 });
+gsap.registerPlugin(DrawSVGPlugin, MorphSVGPlugin,GSDevTools);
 
+
+gsap.set("#spotify_circle", { rotate: 180,transformOrigin: "50% 50%",opacity:0 });
+gsap.set("#spotify_t_l", { drawSVG: "100% 100%", opacity: 0 });
+gsap.set("#spotify_m_l", { drawSVG: "0% 0%", opacity: 0});
+gsap.set("#spotify_b_l", { drawSVG: "100% 100%", opacity: 0 });
 gsap.set("#spotify_l_p", { drawSVG: "20% 80%" });
 gsap.set("#spotify_m_p", { drawSVG: "20% 100%" });
 gsap.set("#spotify_r_p", { drawSVG: "20% 80%" });
@@ -28,6 +27,7 @@ function lineOneAnimation() {
     .to("#spotify_l_p", { duration:0.2,drawSVG: "10% 90%" })
     .to("#spotify_l_p", { duration:0.3,drawSVG: "15% 85%" })
     .to("#spotify_l_p", { duration:0.3,drawSVG: "0% 100%" })
+    .to("#spotify_l_p", { duration:0.3,opacity: "0" },"+=0.3")
     return tl
 
 }
@@ -41,9 +41,8 @@ function lineTwoAnimation() {
     .to("#spotify_m_p", { duration:0.2,drawSVG: "30% 90%" })
     .to("#spotify_m_p", { duration:0.3,drawSVG: "35% 85%" })
     .to("#spotify_m_p", { duration:0.3,drawSVG: "20% 100%" })
-    .to("#spotify_m_p", { duration:0.3,drawSVG: "0% 100%"  ,ease: "sine.inOut"})
-    .to("#spotify_circle", { duration:0.6,drawSVG: "0% 100%"  ,ease: "sine.inOut"})
-    .to("#spotify_circle", { duration:0.6,fill: "#23da69", strokeWidth: 0 ,ease: "sine.inOut"})
+    .to("#spotify_m_p", { duration:0.2,drawSVG: "0% 100%"  ,ease: "sine.inOut"})
+    .to("#spotify_m_p", { duration:0.5,morphSVG: "#spotify_circle",fill: "#23da69", strokeWidth: 0  ,ease: "sine.inOut"})
     return tl
 
 }
@@ -57,6 +56,7 @@ function lineThreeAnimation() {
     .to("#spotify_r_p", { duration:0.2,drawSVG: "10% 90%" })
     .to("#spotify_r_p", { duration:0.3,drawSVG: "15% 85%" })
     .to("#spotify_r_p", { duration:0.3,drawSVG: "0% 100%" })
+    .to("#spotify_r_p", { duration:0.3,opacity: "0" },"+=0.3")
 
     return tl
 
@@ -65,7 +65,9 @@ function tmbLine() {
   var tl = gsap.timeline();
     tl
     .to("#spotify_t_l,#spotify_m_l,#spotify_b_l", { duration: 0.2,opacity: 1})
-    .to("#spotify_t_l,#spotify_m_l,#spotify_b_l", { duration:0.6,x:0,y:0,stagger: 0.05,ease: "sine.inOut"})
+    .to("#spotify_b_l", { duration:0.2,drawSVG: "0% 100%",ease: "sine.inOut"})
+    .to("#spotify_m_l", { duration:0.2,drawSVG: "0% 100%",ease: "sine.inOut"},"-=0.1")
+    .to("#spotify_t_l", { duration:0.2,drawSVG: "0% 100%",ease: "sine.inOut"},"-=0.1")
 
     return tl
 
@@ -73,7 +75,7 @@ function tmbLine() {
 function circleGroup() {
   var tl = gsap.timeline();
     tl
-    .to("#spotify_circle_p_grp", { duration:0.6,x:-65,ease: "sine.inOut"})
+    .to("#spotify_circle_p_grp", { duration:0.4,x:-65,ease: "sine.inOut"})
 
     return tl
 
@@ -81,7 +83,7 @@ function circleGroup() {
 function spotify() {
   var tl = gsap.timeline();
     tl
-    .to("#spotify_s,#spotify_p_l,#spotify_p_o,#spotify_o,#spotify_t_h,#spotify_t_lw,#spotify_i_l,#spotify_f_h,#spotify_f_l,#spotify_y_fs,#spotify_y_bs", { duration:1.5,drawSVG: "201% 301%", stagger: 0.3 })
+    .to("#spotify_s,#spotify_p_l,#spotify_p_o,#spotify_o,#spotify_t_h,#spotify_t_lw,#spotify_i_l,#spotify_f_h,#spotify_f_l,#spotify_y_fs,#spotify_y_bs", { duration:0.6,drawSVG: "0% 100%", stagger: 0.1 })
     .to("#spotify_i_o", { duration:0.5,scale: 1},"-=1")
 
     return tl
@@ -96,4 +98,5 @@ mainTimeline.add(lineOneAnimation(),0)
 .add(circleGroup())
 .add(spotify())
 
-// GSDevTools.create()
+
+GSDevTools.create()
